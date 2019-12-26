03c2fae2823eb100e5ac54cd8cf5e70c90178dea
Melissa Real <melissa.real@hotmail.com>
2019-12-26 14:32:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (objArr) => {
+  const results = objArr.find(x => x.result ==="W")
+  
+ return results ? results.year : undefined
+
+}
